c7b2cb5614a1184134eb449fc192cfb99a856cc7
Nikhil Raghavendra <nikhil@nikhils-macbook-pro.local>
2018-05-04 15:22:34 +0800

    Initial Commit

---
+++ PlaceVisionTests/PlaceVisionTests.swift
@@ -0,0 +1,36 @@
+//
+//  PlaceVisionTests.swift
+//  PlaceVisionTests
+//
+//  Created by Nikhil Raghavendra on 4/5/18.
+//  Copyright © 2018 Nikhil Raghavendra. All rights reserved.
+//
+
+import XCTest
+@testable import PlaceVision
+
+class PlaceVisionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
